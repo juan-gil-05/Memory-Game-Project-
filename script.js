f2a39565634e6ce6a527c075f61f7c842dabb4e8
Juan Esteban Gil Martínez <gilj06@hotmail.com>
2024-03-14 14:14:26 +0100

    ajout du chronometrè sans relation avec les cartes du jeu

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Tableu avec les images des cartes
 const cards = [
     'https://picsum.photos/id/237/100/100',
@@ -88,3 +87,62 @@ function onCardClick(e) {
     }
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
+//Le chronomètre
+
+//variables dont on a besoin
+let sp, btn_start, btn_stop, t, h, mn, s, ms
+//function pour initialiser les variables quand la page se récharche
+window.onload = function(){
+    sp = document.getElementsByTagName("span")
+    btn_start = document.querySelector('.start')
+    btn_stop = document.querySelector('.stop')
+    t
+    h=0, mn=0, s=0, ms=0
+}
+//metre en place le compteur
+function update_chrono(){
+    ms+=1
+    if(ms == 10){
+        ms = 1
+        s+=1
+    }
+    if(s == 60){
+        s = 0
+        mn+=1
+    }
+    if(mn == 60){
+        mn = 0
+        h+=1
+    }   
+    //insertion des valeurs dans les span, 
+    //dont span[0] c'es le première span et ainsi de suite
+    sp[0].innerHTML = h + "h"
+    sp[1].innerHTML = mn + "mn"
+    sp[2].innerHTML = s + "s"
+    sp[3].innerHTML = ms + "ms"
+}
+//Mettre en place la fonction du buton_start
+function start(){
+    //cette ligne de code execute la fonction update_chrono
+    //Tous les 100 ms
+    t = setInterval(update_chrono, 100)
+    btn_start.disabled = true
+}
+//stopper le chronomètre 
+function stop(){
+    clearInterval(t) //supression de l'intervale t que nous avions crée
+    btn_start.disabled = false
+}
+//initialisér les valeurs du compteur
+function reset(){
+    clearInterval(t)
+    btn_start.disabled = false
+    h=0, mn=0, s=0, ms=0
+    //insérer ces nouvelles valeurs dans les span
+    sp[0].innerHTML = h + "h"
+    sp[1].innerHTML = mn + "mn"
+    sp[2].innerHTML = s + "s"
+    sp[3].innerHTML = ms + "ms"
+}
